7b8b3e60f21bf4847703b98edcb722f30691f081
neolgu <32592676+neolgu@users.noreply.github.com>
2024-05-16 16:36:35 +0900

    Update utils.py
    Change so that only specified models are instantiated

--- framework/models/utils.py
+++ framework/models/utils.py
@@ -1,14 +1,14 @@
 from .model_1 import BaseCNN
 
 
-def get_model(model_name):
+def get_model(model_name, **kwargs):
     model_classes = {
-        "BaseCNN": BaseCNN(),
+        "BaseCNN": BaseCNN,
         # other model...
     }
 
     # 주어진 모델 이름이 있는지 확인하고 해당 모델 클래스를 반환합니다.
     if model_name in model_classes:
-        return model_classes[model_name]
+        return model_classes[model_name](**kwargs)
     else:
         raise ValueError(f"Unsupported model: {model_name}")
